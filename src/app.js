0a2eabb6484676c43abd80426ac096bd29ec99c0
Danushka Chathuranga <xdanushka@gmail.com>
2020-08-03 15:44:09 +0530

    setup formating for amount n created at

--- src/app.js
+++ src/app.js
@@ -12,49 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-const unsubscribe = store.subscribe(()=>{
-    const state = store.getState();
-    const vexpenses = getVisibleExpenses(state.expenses, state.filters)
-    // console.log(vexpenses)
-})
-
-var expense = store.dispatch(addExpense(
-    {
-        description: 'rent',
-        note: 'test note',
-        amount: 50,
-        createdAt: 100
-    }
-));
-
-store.dispatch(addExpense(
-    {
-        description: 'other',
-        note: 'other note',
-        amount: 500,
-        createdAt: -1000
-    }
-))
-
-store.dispatch(addExpense(
-    {
-        description: 'water bill',
-        note: 'january water bill',
-        amount: 250,
-        createdAt: 1000
-    }
-))
-
-
-//store.dispatch(removeExpense({id: expense.expense.id}));
-//store.dispatch(editExpense(expense.expense.id, {amount:100}));
-
-//store.dispatch(setTextFilter('rent'));
-//store.dispatch(sortByDate());
-//store.dispatch(sortByAmount());
-//store.dispatch(setStartDate(125));
-
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
